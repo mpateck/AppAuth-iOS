1346d9b474daaf8376a68f3202b418722591fa55
Matthias Pateck <mpateck@fum.de>
2018-08-10 07:46:29 +0200

    Compiler errors fixed

--- Source/OIDAuthState.m
+++ Source/OIDAuthState.m
@@ -153,7 +153,7 @@ static const NSUInteger kExpiryTimeTolerance = 60;
     OID_UNAVAILABLE_USE_INITIALIZER(@selector(initWithAuthorizationResponse:tokenResponse:));
 
 /*! @brief Creates an auth state from an authorization response.
-    @param response The authorization response.
+ @param authorizationResponse The authorization response.
  */
 - (instancetype)initWithAuthorizationResponse:(OIDAuthorizationResponse *)authorizationResponse {
   return [self initWithAuthorizationResponse:authorizationResponse tokenResponse:nil];
@@ -161,7 +161,6 @@ static const NSUInteger kExpiryTimeTolerance = 60;
 
 
 /*! @brief Designated initializer.
-    @param response The authorization response.
     @discussion Creates an auth state from an authorization response and token response.
  */
 - (instancetype)initWithAuthorizationResponse:(OIDAuthorizationResponse *)authorizationResponse
@@ -472,25 +471,25 @@ static const NSUInteger kExpiryTimeTolerance = 60;
     dispatch_async(dispatch_get_main_queue(), ^() {
       // update OIDAuthState based on response
       if (response) {
-        _needsTokenRefresh = NO;
+          self->_needsTokenRefresh = NO;
         [self updateWithTokenResponse:response error:nil];
       } else {
         if (error.domain == OIDOAuthTokenErrorDomain) {
-          _needsTokenRefresh = NO;
+            self->_needsTokenRefresh = NO;
           [self updateWithAuthorizationError:error];
         } else {
-          if ([_errorDelegate respondsToSelector:
+            if ([self->_errorDelegate respondsToSelector:
               @selector(authState:didEncounterTransientError:)]) {
-            [_errorDelegate authState:self didEncounterTransientError:error];
+                [self->_errorDelegate authState:self didEncounterTransientError:error];
           }
         }
       }
 
       // nil the pending queue and process everything that was queued up
       NSArray *actionsToProcess;
-      @synchronized(_pendingActionsSyncObject) {
-        actionsToProcess = _pendingActions;
-        _pendingActions = nil;
+        @synchronized(self->_pendingActionsSyncObject) {
+            actionsToProcess = self->_pendingActions;
+            self->_pendingActions = nil;
       }
       for (OIDAuthStateAction actionToProcess in actionsToProcess) {
         actionToProcess(self.accessToken, self.idToken, error);
